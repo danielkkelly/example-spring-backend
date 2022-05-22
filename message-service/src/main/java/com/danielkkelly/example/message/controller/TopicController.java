56a7aa03757d40ffcd5e1e0b7e845ee85ead78d0
Dan Kelly <dan@boxcarapp.com>
2022-05-22 17:14:40 -0400

    Started slimming down security-service's pom.  Updated topic service and controller with basic find and save

--- message-service/src/main/java/com/danielkkelly/example/message/controller/TopicController.java
+++ message-service/src/main/java/com/danielkkelly/example/message/controller/TopicController.java
@@ -1,9 +1,29 @@
 package com.danielkkelly.example.message.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.danielkkelly.example.message.domain.model.Topic;
+import com.danielkkelly.example.message.service.TopicService;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.inject.Inject;
 
 @RestController
 public class TopicController {
 
-   //todo: create a parent pom and share the @inject annotation
+    @Inject
+    private TopicService service;
+    @GetMapping("/topics")
+    public Flux findAllTopics() {
+        return service.findAll();
+    }
+
+    @GetMapping("/topics/{id}")
+    public Mono findById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+    @PostMapping("/topics")
+    public Mono create(@RequestBody Topic topic) {
+        return service.save(topic);
+    }
 }
--- message-service/src/main/java/com/danielkkelly/example/message/service/TopicService.java
+++ message-service/src/main/java/com/danielkkelly/example/message/service/TopicService.java
@@ -1,4 +1,28 @@
 package com.danielkkelly.example.message.service;
 
-public class TopicService {
+import com.danielkkelly.example.message.domain.model.Topic;
+import com.danielkkelly.example.message.repository.TopicRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.inject.Inject;
+
+@Service
+public class TopicService<T extends Topic> {
+
+    @Inject
+    private TopicRepository repository;
+
+    public Flux<Topic> findAll() {
+       return repository.findAll();
+    }
+
+    public Mono<Topic> findById(final Long id) {
+        return repository.findById(id);
+    }
+
+    public Mono<Topic> save(final Topic topic) {
+        return repository.save(topic);
+    }
 }
