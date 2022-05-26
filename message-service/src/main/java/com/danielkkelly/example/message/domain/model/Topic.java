c6bda90935820a3226f486a677f9d8b454ad1f43
Dan Kelly <dan@ip-10-10-110-11.ec2.internal>
2022-05-26 16:37:14 -0400

    Updates topic with ID column details, adds .java-version

--- message-service/src/main/java/com/danielkkelly/example/message/domain/model/Topic.java
+++ message-service/src/main/java/com/danielkkelly/example/message/domain/model/Topic.java
@@ -3,6 +3,8 @@ package com.danielkkelly.example.message.domain.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
 import java.util.Date;
 
@@ -10,6 +12,8 @@ import java.util.Date;
 @Setter
 @NoArgsConstructor
 public class Topic {
+    @Id
+    @Column("topic_id")
     private Long id;
     private String topicName;
     private Date createdDate;
