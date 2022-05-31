12fd0a742992e5ef6e5f297ab1ff1fd0e30c141e
Dan Kelly <dan@boxcarapp.com>
2022-05-30 20:26:02 -0400

    Updates to use @Data since we aren't using JPA

--- message-service/src/main/java/com/danielkkelly/example/message/domain/model/Topic.java
+++ message-service/src/main/java/com/danielkkelly/example/message/domain/model/Topic.java
@@ -1,24 +1,20 @@
 package com.danielkkelly.example.message.domain.model;
 
 import com.danielkkelly.example.commons.domain.model.R2dbcStamped;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@NoArgsConstructor
+@Data
 public class Topic implements R2dbcStamped {
     @Id
     @Column("topic_id")
     private Long id;
-    @NotNull
+    @NotBlank
     @Size(max = 100)
     private String topicName;
     private LocalDateTime createdDate;
