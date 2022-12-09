34b80addb83ed107d5ecc505de8a3db49a2b60ee
dan <dan@aahcs.org>
2022-12-09 10:48:46 -0500

    Updates the script to connect to the example database before altering default privileges

--- db-setup-pgsql.sql
+++ db-setup-pgsql.sql
@@ -5,6 +5,8 @@ create user example_ro with encrypted password 'example_ro';
 
 create database example;
 
+\connect example;
+
 alter default privileges in schema public grant all privileges on tables TO example_ddl;
 alter default privileges for role example_ddl in schema public grant select, insert, update, delete on tables TO example;
 alter default privileges for role example_ddl in schema public grant select on tables TO example_ro;
